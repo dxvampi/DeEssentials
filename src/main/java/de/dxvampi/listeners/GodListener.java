a3e51443ca2d34b99469beca1cfc5bb63ec0d03a
dxvampi <dxvampi@gmail.com>
2026-06-17 17:09:24 +0200

    Removed random import

--- src/main/java/de/dxvampi/listeners/GodListener.java
+++ src/main/java/de/dxvampi/listeners/GodListener.java
@@ -4,7 +4,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 
 public class GodListener implements Listener {
     @EventHandler
